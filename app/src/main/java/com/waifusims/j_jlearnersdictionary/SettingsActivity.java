68741f24470385983b7933f88978733859c98074
Limegrass <jani@davidson.edu>
2018-05-19 21:22:39 -0500

    Settings Activity and resources
    Implemented a ListPreference in Settings Activity for which dictionary type to search, associated resources

--- app/src/main/java/com/waifusims/j_jlearnersdictionary/SettingsActivity.java
+++ app/src/main/java/com/waifusims/j_jlearnersdictionary/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.waifusims.j_jlearnersdictionary;
 
+import android.app.ActionBar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ActionBar actionBar = this.getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
