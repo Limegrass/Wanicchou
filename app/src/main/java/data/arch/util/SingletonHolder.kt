ecd074bbc249e0d943c87be1a82428073982f8f9
Limegrass <multipresident@gmail.com>
2019-07-29 00:48:41 -0500

    Cleaner SingletonHolder Definition
    Cleaned up implementation. Though I kind of hate using this class regardless.

--- app/src/main/java/data/arch/util/SingletonHolder.kt
+++ app/src/main/java/data/arch/util/SingletonHolder.kt
@@ -1,26 +1,13 @@
 package data.arch.util
 
-open class SingletonHolder<out T, in A> (creator: (A) -> T) {
-    private var creator: ((A) -> T)? = creator
+open class SingletonHolder<out T, in A> (private val creator: (A) -> T) {
     @Volatile private var instance: T? = null
-    fun getInstance(arg: A) : T {
-        val localInstance = instance
-        if (localInstance != null) {
-            return localInstance
+    // Double checked locking on the creator
+    protected fun getInstance(arg: A) : T {
+        instance = instance ?: synchronized(creator){
+            instance = instance ?: creator(arg)
+            instance
         }
-
-        return synchronized(this){
-            val syncedInstance = instance
-            if (syncedInstance != null) {
-                syncedInstance
-            }
-            else {
-                val created = creator!!(arg)
-                instance = created
-                creator = null
-                created
-            }
-        }
+        return instance!!
     }
 }
-
